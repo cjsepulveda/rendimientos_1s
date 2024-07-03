67d16b24fb8358260d22d9ca645faeec45953b6c
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-03 18:16:36 -0400

    lineas extras eliminadas

--- src/app.py
+++ src/app.py
@@ -1,31 +1,22 @@
 import pathlib
-import dash
-from dash import dcc
-from dash import html
+from dash import Dash, html, dcc
 import plotly.graph_objs as go
 import pandas as pd
 import plotly.express as px
 
-
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
-df = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
+df01 = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
 
-trace5 = px.bar(df, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
+trace01 = px.bar(df01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
 
-
-#fig = go.Figure(data=[
- #   go.Bar(name='SF Zoo', x=animals, y=[20, 14, 23]),
-  #  (name='LA Zoo', x=animals, y=[12, 18, 29])
-# ])
-
-app = dash.Dash()
+app = Dash(__name__)
 server=app.server
 
 app.layout = html.Div(children=[
-    html.H1(children='Reporte 1° Medios Año 2024'),
+    html.H1(children='Reporte 1° Medios 2024'),
     html.Div(children='Graficas'),
-    dcc.Graph(figure=trace5)
+    dcc.Graph(figure=trace01)
 ])
 
 
