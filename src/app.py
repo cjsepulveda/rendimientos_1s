9678fe92275766ea03242f6dd48ea92183451bb7
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-03 17:40:33 -0400

    cambie graph a plotly express

--- src/app.py
+++ src/app.py
@@ -4,48 +4,28 @@ from dash import dcc
 from dash import html
 import plotly.graph_objs as go
 import pandas as pd
+import plotly.express as px
 
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
-dfe = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
+df = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
+
+trace5 = px.bar(df, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
 
 
-
-# file_name = "./src/experimento.xlsx" # Give Any Name / Existing Name
-
-
-# dfe = pd.read_excel(file_name, usecols=None, sheet_name='Hoja1') 
-
-
-trace5 = go.Bar(x=dfe.CURSO, y=dfe.Promovidos)
-
-
-
-## Importar la data
-# df = pd.read_csv('./src/data.csv', delimiter = ';')
-
-#Crear una tabla dinámica
-# pv = pd.pivot_table(df, index=['Name'], columns=["Status"], values=['Quantity'], aggfunc="sum", fill_value=0)
-
-# trace1 = go.Bar(x=pv.index, y=pv[('Quantity', 'declinada')], name='Declinada')
-# trace2 = go.Bar(x=pv.index, y=pv[('Quantity', 'pendiente')], name='Pendiente')
-# trace3 = go.Bar(x=pv.index, y=pv[('Quantity', 'presentada')], name='Presentada')
-# trace4 = go.Bar(x=pv.index, y=pv[('Quantity', 'ganada')], name='Ganada')
+#fig = go.Figure(data=[
+ #   go.Bar(name='SF Zoo', x=animals, y=[20, 14, 23]),
+  #  (name='LA Zoo', x=animals, y=[12, 18, 29])
+# ])
 
 app = dash.Dash()
 server=app.server
 
 app.layout = html.Div(children=[
     html.H1(children='Reporte 1° Medios Año 2024'),
-    html.Div(children='''Promovidos.'''),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [trace5],
-            'layout':
-            go.Layout(title='Promovidos 2° Medio', barmode='stack')
-        })
+    html.Div(children='Graficas'),
+    dcc.Graph(figure=trace5)
 ])
 
 
