925688bb8cd2deef4698f561e27c1778401eccf9
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-03 23:28:17 -0400

    cambios codigos

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import pathlib
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.graph_objs as go
 import pandas as pd
 import plotly.express as px
@@ -8,16 +8,37 @@ PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
 df01 = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
 
-trace01 = px.bar(df01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
+
 
 app = Dash(__name__)
 server=app.server
 
 app.layout = html.Div(children=[
+    html.Label('Nivel'),
+        dcc.Dropdown(['1MEDIO', '2MEDIO'], '1MEDIO', id='nivel'),
+        dcc.Dropdown(['LENGUAJE','MATEMÁTICA'],'LENGUAJE', id='subject'),
     html.H1(children='Reporte 1° Medios 2024'),
     html.Div(children='Graficas'),
-    dcc.Graph(figure=trace01)
+    dcc.Graph( id='grafica')
 ])
+
+@app.callback(
+        Output('grafica', 'figure'),
+        Input('nivel', 'value'),
+        Input('subject','value')
+        )
+
+def update_charts(grade,sub):
+
+    gr = grade
+    s = sub
+
+    mask01 = df01.query(
+        "NIVEL == @gr and ASIGNATURA == @s")
+        
+    trace01 = px.bar(mask01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
+
+    return trace01
 
 
 if __name__ == '__main__':
