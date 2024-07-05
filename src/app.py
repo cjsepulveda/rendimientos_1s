27ceccb913d656e6184798eb7984c7ef668f307b
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-05 16:21:36 -0400

    cambios codigo y estilos

--- src/app.py
+++ src/app.py
@@ -1,40 +1,92 @@
 import pathlib
-from dash import Dash, dcc, html, Input, Output, callback
-import plotly.graph_objs as go
+from dash import Dash, dcc, html, Input, Output
 import pandas as pd
 import plotly.express as px
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
 df01 = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
+grades = df01["NIVEL"].sort_values().unique()
+subjets = df01["ASIGNATURA"].sort_values().unique()
 
 
+# print(df01)
+# mask01=df01[df01["NIVEL"]=="1MEDIO"]
+# print(mask01)
 
 app = Dash(__name__)
 server=app.server
 
-app.layout = html.Div(children=[
-    html.Label('Nivel'),
-        dcc.Dropdown(['1MEDIO', '2MEDIO'], '1MEDIO', id='nivel'),
-        dcc.Dropdown(['LENGUAJE','MATEMÁTICA'],'LENGUAJE', id='subject'),
-    html.H1(children='Reporte 1° Medios 2024'),
-    html.Div(children='Graficas'),
-    dcc.Graph( id='grafica')
-])
+app.layout = html.Div(
+    children=[
+
+html.Div(
+            children=[
+                html.H1(
+                    children="Gráficas Rendimientos 1° Semestre 2024", className="header-title"
+                ),
+                html.P(
+                    children=(
+                        "Análisis del rendimiento por nivel y asignatura"
+                    ),
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+
+
+html.Div(children=[
+html.Div(
+    children=[
+        html.Div(children="NIVEL", className="menu-title"),
+        dcc.Dropdown(
+            id='level', 
+            options=[ 
+                {"label": nivel, "value": nivel} 
+                for nivel in grades
+            ],
+            value="1MEDIO",
+            clearable=False,
+            
+        ),
+    ]),
+html.Div(
+    children=[
+        html.Div(children="ASIGNATURA", className="menu-title"),
+        dcc.Dropdown(
+            id='subject',
+            options=[
+                {"label": asignatura, "value": asignatura}
+                for asignatura in subjets
+            ],
+            value="LENGUAJE",
+            clearable=False,
+                ),
+    
+]),
+],
+className="menu",
+),
+
+html.Div(children=[
+    dcc.Graph( id='grafica', config={"displayModeBar": False}, className="card")
+],
+className="wrapper",
+),
+    ])
+
 
 @app.callback(
         Output('grafica', 'figure'),
-        Input('nivel', 'value'),
+        Input('level', 'value'),
         Input('subject','value')
         )
 
-def update_charts(grade,sub):
-
-    gr = grade
-    s = sub
+def update_charts(nivel,asignatura):
 
     mask01 = df01.query(
-        "NIVEL == @gr and ASIGNATURA == @s")
+        "NIVEL == @nivel and ASIGNATURA == @asignatura")
         
     trace01 = px.bar(mask01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
 
