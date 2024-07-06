fac93a5e16144f7452d022805d3198b222de1a5b
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-06 17:09:10 -0400

    cambie diccionarios

--- src/app.py
+++ src/app.py
@@ -13,10 +13,12 @@ subjetsALL = df01["ASIGNATURA"].sort_values().unique()
 mask01 = df01[df01["NIVEL"]=="1MEDIO"]
 mask02 = df01[df01["NIVEL"]=="2MEDIO"]
 mask03 = df01[df01["NIVEL"]=="3MEDIO"]
+mask04 = df01[df01["NIVEL"]=="4MEDIO"]
 
 subjets1M = mask01["ASIGNATURA"].sort_values().unique()
 subjets2M = mask02["ASIGNATURA"].sort_values().unique()
 subjets3M = mask03["ASIGNATURA"].sort_values().unique()
+subjets4M = mask04["ASIGNATURA"].sort_values().unique()
 
 # print(df01)
 # mask01=df01[df01["NIVEL"]=="1MEDIO"]
@@ -28,7 +30,8 @@ server=app.server
 all_options = {
     '1MEDIO': subjets1M,
     '2MEDIO': subjets2M,
-    '3MEDIO': subjets3M
+    '3MEDIO': subjets3M,
+    '4MEDIO': subjets4M
 }
 
 app.layout = html.Div(
@@ -38,14 +41,7 @@ html.Div(
             children=[
                 html.H1(
                     children="Gráficas Rendimientos 1° Semestre 2024", className="header-title"
-                ),
-                html.P(
-                    children=(
-                        "Análisis del rendimiento por nivel y asignatura"
-                    ),
-                    className="header-description",
-                ),
-            ],
+                )],
             className="header",
         ),
 
@@ -53,18 +49,18 @@ html.Div(
 html.Div(children=[
 html.Div(
     children=[
-        html.Div(children="NIVEL", className="menu-title"),
+        html.Div(children='NIVEL', className='menu-title'),
         dcc.Dropdown(
             id='level', 
             options=[ 
-                {"label": nivel, "value": nivel}
+                {'label': nivel, 'value': nivel}
                 
                 for nivel in all_options.keys()
                 
             ],
-            value="1MEDIO",
+            value='1MEDIO',
             clearable=False,
-            
+            className='dropdown'
         ),
     ]),
 html.Div(
@@ -72,13 +68,14 @@ html.Div(
         html.Div(children="ASIGNATURA", className="menu-title"),
         dcc.Dropdown(
             id='subject',
-            options=[
-                {"label": asignatura, "value": asignatura}
+           # options=[
+            #    {"label": asignatura, "value": asignatura}
                                 
-                for asignatura in all_options['1MEDIO']
-            ],
-            value="LENGUAJE",
+             #   for asignatura in all_options['1MEDIO']
+            #],
+            # value="LENGUAJE",
             clearable=False,
+            className='dropdown'
                 ),
     
 ]),
@@ -93,6 +90,21 @@ className="wrapper",
 ),
     ])
 
+@app.callback(
+    Output('subject', 'options'),
+    Output('subject', 'value'),
+    Input('level', 'value')
+    )
+
+def set_subject_options(selected_level):
+
+    options_level = [{'label': i, 'value': i} 
+                    for i in all_options[selected_level]]
+    
+    options_value = all_options[selected_level][0]
+
+    return options_level, options_value
+
 
 @app.callback(
         Output('grafica', 'figure'),
@@ -105,7 +117,14 @@ def update_charts(nivel,asignatura):
     mask01 = df01.query(
         "NIVEL == @nivel and ASIGNATURA == @asignatura")
         
-    trace01 = px.bar(mask01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'], barmode='group')
+    trace01 = px.bar(mask01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'],
+                     title= 'Rendimientos estudiantes',
+                     width=1024, height=400,
+                     labels={'value':'Cantidad estudiantes','variable':'Categorías','CURSO':'Cursos'},
+                     barmode='group',
+                     color_discrete_map={'Matrícula':'blue','Promovidos':'green','Reprobados':'orange'},
+                     template="simple_white",
+                     )
 
     return trace01
 
