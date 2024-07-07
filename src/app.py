ba4569f2c2dbdb10a7124cd013e38abfd4172454
carlosjaviersepulveda <cj.sepulveda.olivares@gmail.com>
2024-07-07 19:26:04 -0400

    cambios base datos y codigo

--- src/app.py
+++ src/app.py
@@ -5,20 +5,20 @@ import plotly.express as px
 
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
-df01 = pd.read_excel(DATA_PATH.joinpath('experimento.xlsx'), sheet_name='Hoja1')
+df01 = pd.read_excel(DATA_PATH.joinpath('data_level_grade.xlsx'), sheet_name='DATA')
 
 grades = df01["NIVEL"].sort_values().unique()
-subjetsALL = df01["ASIGNATURA"].sort_values().unique()
+subjetsALL = df01["ASIGNATURA"].unique()
 
 mask01 = df01[df01["NIVEL"]=="1MEDIO"]
 mask02 = df01[df01["NIVEL"]=="2MEDIO"]
 mask03 = df01[df01["NIVEL"]=="3MEDIO"]
 mask04 = df01[df01["NIVEL"]=="4MEDIO"]
 
-subjets1M = mask01["ASIGNATURA"].sort_values().unique()
-subjets2M = mask02["ASIGNATURA"].sort_values().unique()
-subjets3M = mask03["ASIGNATURA"].sort_values().unique()
-subjets4M = mask04["ASIGNATURA"].sort_values().unique()
+subjets1M = mask01["ASIGNATURA"].unique()
+subjets2M = mask02["ASIGNATURA"].unique()
+subjets3M = mask03["ASIGNATURA"].unique()
+subjets4M = mask04["ASIGNATURA"].unique()
 
 # print(df01)
 # mask01=df01[df01["NIVEL"]=="1MEDIO"]
@@ -75,7 +75,7 @@ html.Div(
             #],
             # value="LENGUAJE",
             clearable=False,
-            className='dropdown'
+            className='dropdown',
                 ),
     
 ]),
@@ -96,7 +96,7 @@ className="wrapper",
     Output('subject', 'value'),
     Input('level', 'value')
     )
-
+# función para cambiar opciones de lista despegable asignaturas segun el nivel y valor inicial
 def set_subject_options(selected_level):
 
     options_level = [{'label': i, 'value': i} 
@@ -113,20 +113,33 @@ def set_subject_options(selected_level):
         Input('subject','value')
         )
 
+# función para grafico segun nivel y asignatura
 def update_charts(nivel,asignatura):
 
-    mask01 = df01.query(
+    select_nivel_subject = df01.query(
         "NIVEL == @nivel and ASIGNATURA == @asignatura")
         
-    trace01 = px.bar(mask01, x='CURSO', y=['Matrícula','Promovidos','Reprobados'],
+    trace01 = px.bar(select_nivel_subject, x='CURSO', y=['MB','B','S','I','P'],
                      title= 'Rendimientos estudiantes',
-                     width=1024, height=400,
-                     labels={'value':'Cantidad estudiantes','variable':'Categorías','CURSO':'Cursos'},
+                     width=1200, height=400,
+                     labels={'value':'Porcentaje estudiantes','variable':'Categorías','CURSO':'Cursos'},
                      barmode='group',
-                     color_discrete_map={'Matrícula':'blue','Promovidos':'green','Reprobados':'orange'},
+                     color_discrete_map={'MB':'blue','B':'green','S':'orange','I':'tomato','P':'gold'},
                      template="simple_white",
+                     text_auto='.0%',
+                     range_y=[0,1],
                      )
-
+    
+    trace01.update_yaxes(tickformat=".1%", tickfont_weight='bold',title_font_weight='bold')
+    trace01.update_xaxes(tickfont_weight='bold')
+    trace01.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+    trace01.update_layout(
+                         hoverlabel_font_color='white',
+                         uniformtext_minsize=5,
+                         uniformtext_mode='show',
+                         title_font_weight='bold'
+                         )
+    
     return trace01
 
 
